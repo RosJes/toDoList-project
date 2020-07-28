0adaaee611efd8e1452731a578548d7dd615e43f
RosJes <isaksson.jessica@gmail.com>
2020-07-28 13:53:08 +0200

    css

---
+++ wwwroot/script/site.js
@@ -0,0 +1,18 @@
+﻿let counter = 0;
+function Check(id) {
+    console.log(counter)
+    var att = document.createAttribute("class"); 
+    att.value = "checked";     
+    let x = document.getElementById(id)
+    let y = document.getElementsByTagName('LI')[id];
+    if (counter % 2 == 0) {
+        x.innerText = 'true'
+       y.setAttributeNode(att);           
+       
+    }
+    else
+        x.innerText = 'false'
+
+    counter++
+}
+
