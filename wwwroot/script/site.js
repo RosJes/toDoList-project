fb8f0ef872357bef2cc24f4c1c87793bce46feac
RosJes <isaksson.jessica@gmail.com>
2020-07-28 15:04:03 +0200

    line trough

--- wwwroot/script/site.js
+++ wwwroot/script/site.js
@@ -1,18 +1,20 @@
 ﻿let counter = 0;
 function Check(id) {
     console.log(counter)
-    var att = document.createAttribute("class"); 
+    let att = document.createAttribute("class"); 
     let x = document.getElementById(id)
     let li = document.getElementById('item '+id)
     if (counter % 2 == 0) {
         x.innerText = 'true'
-        att.value = "checked";    
-        li.setAttributeNode(att)
+        //att.value = "checked";    
+        //li.setAttributeNode(att)
+        li.style.textDecoration = 'line-through'
     }
     else {
         x.innerText = 'false'
-        att.value='unchecked'
-        li.setAttributeNode(att)
+        //att.value='unchecked'
+        //li.setAttributeNode(att)
+        li.style.textDecoration = 'none'
     }
     counter++
 }
