179640766c45def99dc46393bb90b92b6d3d344e
RosJes <isaksson.jessica@gmail.com>
2020-07-28 15:20:44 +0200

    list style

--- wwwroot/script/site.js
+++ wwwroot/script/site.js
@@ -1,19 +1,13 @@
 ﻿let counter = 0;
 function Check(id) {
-    console.log(counter)
-    let att = document.createAttribute("class"); 
     let x = document.getElementById(id)
     let li = document.getElementById('item '+id)
     if (counter % 2 == 0) {
         x.innerText = 'true'
-        //att.value = "checked";    
-        //li.setAttributeNode(att)
         li.style.textDecoration = 'line-through'
     }
     else {
         x.innerText = 'false'
-        //att.value='unchecked'
-        //li.setAttributeNode(att)
         li.style.textDecoration = 'none'
     }
     counter++
